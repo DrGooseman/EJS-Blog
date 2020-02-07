7da23085d167fdd65fcc6ba3d8e4419ea740e4e9
DrGooseman <jamesjquinn12@gmail.com>
2020-02-07 12:22:18 +0300

    typing in /posts/post will now take you to a page that displays post, if it exists

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -36,6 +35,17 @@ app.post("/compose", function(req, res) {
   const post = { title: req.body.title, post: req.body.post };
   posts.push(post);
   res.redirect("/");
+});
+app.get("/posts/:page", function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.page);
+  let post;
+  posts.forEach(function(element) {
+    if (_.lowerCase(element.title) === requestedTitle) {
+      post = element;
+    }
+  });
+  if (post) res.render("post", { title: post.title, body: post.post });
+  else console.log("FAIL");
 });
 
 app.listen(3000, function() {
